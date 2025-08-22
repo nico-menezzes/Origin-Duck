7b95e9a36ca31a4c7d56e830f0bb7c9bf6738c99
Nico Menezes <contato@nicomenezes.com>
2025-08-22 18:55:02 -0300

    Atualizando script com nova mensagem de console

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 // Origin - Duck Script
 // Arquivo JavaScript criado para o repositório Origin-Duck
 
-console.log('Origin - Duck está funcionando!');
-
-// Função de exemplo
-function duckFunction() {
-    return 'Quack! Quack!';
-}
-
-// Exportar função para uso em outros módulos
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = { duckFunction };
-}
+console.log('estou funcionando');
