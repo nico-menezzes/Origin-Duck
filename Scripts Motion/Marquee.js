80aa7a2e130ddcdb0728a9c02624daf5523bdbf2
Nico Menezes <contato@nicomenezes.com>
2025-08-29 15:33:15 -0300

    Create Marquee.js

---
+++ Scripts Motion/Marquee.js
@@ -0,0 +1,89 @@
+<script>
+    window.Webflow ||= [];
+    window.Webflow.push(() => {
+        const $marquees = $('[marquee="wrapper"]');
+        
+        $marquees.each(function () {
+            const $marquee = $(this);
+            const $marqueeList = $marquee.find('[marquee="list"]');
+            const $listParent = $marqueeList.parent();
+            const baseDuration = parseFloat($marquee.attr('marquee-duration')) || 60;
+            const direction = ($marquee.attr('marquee-direction') || 'left').toLowerCase();
+            
+            // Determine animation direction
+            let fromX, toX;
+            if (direction === 'right') {
+                fromX = '0%';
+                toX = '100%';
+            } else {
+                // Default to left
+                fromX = '0%';
+                toX = '-100%';
+            }
+            
+            // Duplicate the marquee list 6 times (original + 5 clones = 6 total)
+            for (let i = 0; i < 5; i++) {
+                $marqueeList.clone().appendTo($listParent);
+            }
+            
+            // Get all lists
+            const $allLists = $marquee.find('[marquee="list"]');
+            
+            // Create timeline
+            const tl = gsap.timeline({ repeat: -1 });
+            tl.fromTo(
+                $allLists,
+                { x: fromX },
+                {
+                    x: toX,
+                    ease: "linear",
+                    duration: baseDuration
+                }
+            );
+        });
+    });
+</script><script>
+    window.Webflow ||= [];
+    window.Webflow.push(() => {
+        const $marquees = $('[marquee="wrapper"]');
+        
+        $marquees.each(function () {
+            const $marquee = $(this);
+            const $marqueeList = $marquee.find('[marquee="list"]');
+            const $listParent = $marqueeList.parent();
+            const baseDuration = parseFloat($marquee.attr('marquee-duration')) || 60;
+            const direction = ($marquee.attr('marquee-direction') || 'left').toLowerCase();
+            
+            // Determine animation direction
+            let fromX, toX;
+            if (direction === 'right') {
+                fromX = '0%';
+                toX = '100%';
+            } else {
+                // Default to left
+                fromX = '0%';
+                toX = '-100%';
+            }
+            
+            // Duplicate the marquee list 6 times (original + 5 clones = 6 total)
+            for (let i = 0; i < 5; i++) {
+                $marqueeList.clone().appendTo($listParent);
+            }
+            
+            // Get all lists
+            const $allLists = $marquee.find('[marquee="list"]');
+            
+            // Create timeline
+            const tl = gsap.timeline({ repeat: -1 });
+            tl.fromTo(
+                $allLists,
+                { x: fromX },
+                {
+                    x: toX,
+                    ease: "linear",
+                    duration: baseDuration
+                }
+            );
+        });
+    });
+</script>
